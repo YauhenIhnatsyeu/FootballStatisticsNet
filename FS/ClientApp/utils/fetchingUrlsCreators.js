91023da6faf8b612d9728b90c34603a3d183f150
Yauhen Ihnatsyeu <greatdoubletbpot@gmail.com>
2018-04-10 13:24:13 +0300

    Replaced 'http' with 'https'

--- FS/ClientApp/utils/fetchingUrlsCreators.js
+++ FS/ClientApp/utils/fetchingUrlsCreators.js
@@ -1,19 +1,19 @@
 export function createLeagueUrl(leagueId) {
-    return `http://api.football-data.org/v1/competitions/${leagueId}/leagueTable`;
+    return `//api.football-data.org/v1/competitions/${leagueId}/leagueTable`;
 }
 
 export function createTeamsUrl(leagueId) {
-    return `http://api.football-data.org/v1/competitions/${leagueId}/teams`;
+    return `//api.football-data.org/v1/competitions/${leagueId}/teams`;
 }
 
 export function createTeamUrl(teamId) {
-    return `http://api.football-data.org/v1/teams/${teamId}`;
+    return `//api.football-data.org/v1/teams/${teamId}`;
 }
 
 export function createFixturesUrl(teamId) {
-    return `https://api.football-data.org/v1/teams/${teamId}/fixtures`;
+    return `//api.football-data.org/v1/teams/${teamId}/fixtures`;
 }
 
 export function createHead2HeadUrl(fixtureId) {
-    return `http://api.football-data.org/v1/fixtures/${fixtureId}?head2head=10`;
+    return `//api.football-data.org/v1/fixtures/${fixtureId}?head2head=10`;
 }
