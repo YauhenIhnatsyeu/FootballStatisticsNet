76908f07fae08d25dd6906a499b03ef1f395d7fd
Yauhen Ihnatsyeu <greatdoubletbpot@gmail.com>
2018-04-11 09:51:56 +0300

    Added location.protocol to the beginnings of urls

--- FS/ClientApp/utils/fetchingUrlsCreators.js
+++ FS/ClientApp/utils/fetchingUrlsCreators.js
@@ -1,19 +1,19 @@
 export function createLeagueUrl(leagueId) {
-    return `//api.football-data.org/v1/competitions/${leagueId}/leagueTable`;
+    return `${location.protocol}//api.football-data.org/v1/competitions/${leagueId}/leagueTable`;
 }
 
 export function createTeamsUrl(leagueId) {
-    return `//api.football-data.org/v1/competitions/${leagueId}/teams`;
+    return `${location.protocol}//api.football-data.org/v1/competitions/${leagueId}/teams`;
 }
 
 export function createTeamUrl(teamId) {
-    return `//api.football-data.org/v1/teams/${teamId}`;
+    return `${location.protocol}//api.football-data.org/v1/teams/${teamId}`;
 }
 
 export function createFixturesUrl(teamId) {
-    return `//api.football-data.org/v1/teams/${teamId}/fixtures`;
+    return `${location.protocol}//api.football-data.org/v1/teams/${teamId}/fixtures`;
 }
 
 export function createHead2HeadUrl(fixtureId) {
-    return `//api.football-data.org/v1/fixtures/${fixtureId}?head2head=10`;
+    return `${location.protocol}//api.football-data.org/v1/fixtures/${fixtureId}?head2head=10`;
 }
