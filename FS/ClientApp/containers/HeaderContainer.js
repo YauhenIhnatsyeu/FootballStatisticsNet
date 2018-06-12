ceef7ce9b7c519dd58e1e46392fe087facc7e80b
Yauhen Ihnatsyeu <greatdoubletbpot@gmail.com>
2018-06-12 12:20:49 +0300

    Fixed nav links styles.

--- FS/ClientApp/containers/HeaderContainer.js
+++ FS/ClientApp/containers/HeaderContainer.js
@@ -1,4 +1,5 @@
 import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
 import Header from "Components/header/Header";
 
@@ -7,4 +8,4 @@ const mapStateToProps = state => ({
 });
 
 export default
-connect(mapStateToProps)(Header);
+withRouter(connect(mapStateToProps)(Header));
