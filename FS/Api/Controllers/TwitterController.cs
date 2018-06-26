fbcbc802bb30f2435a299ddb0c1a465e086bcdfa
Yauhen Ihnatsyeu <greatdoubletbpot@gmail.com>
2018-06-26 17:13:21 +0300

    Moved twitter query key and searching url to appsetting.json.

--- FS/Api/Controllers/TwitterController.cs
+++ FS/Api/Controllers/TwitterController.cs
@@ -1,29 +1,36 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using System.Web;
 using FS.Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json.Linq;
 
 namespace FS.Controllers
 {
     public class TwitterController : Controller
     {
+        private readonly IConfiguration configuration;
         private readonly ITwitterService twitterService;
 
-        public TwitterController(ITwitterService twitterService)
+        public TwitterController(ITwitterService twitterService, IConfiguration configuration)
         {
             this.twitterService = twitterService;
+            this.configuration = configuration;
         }
 
         [HttpGet]
         [Route("/api/twitter/search")]
         public async Task<IActionResult> Search()
         {
-            if (!HttpContext.Request.Query.ContainsKey("q")) return BadRequest();
+            string queryKey = configuration["Twitter:QueryKey"];
 
-            string query = HttpUtility.UrlEncode(HttpContext.Request.Query["q"]);
-            string url = "https://api.twitter.com/1.1/search/tweets.json" +
-                         $"?q={query}&result_type=popular&count=10";
+            if (!HttpContext.Request.Query.ContainsKey(queryKey)) return BadRequest();
+
+            string tweetsSearchingUrlTemplate = configuration["Twitter:TweetsSearchingUrl"];
+
+            string query = HttpUtility.UrlEncode(HttpContext.Request.Query[queryKey]);
+            string url = String.Format(tweetsSearchingUrlTemplate, query);
 
             string responseString = await twitterService.SendApiRequestAsync(url);
             var json = JObject.Parse(responseString);
