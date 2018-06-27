f159b0286940843182e95fc14a8dae9dba6fdb56
Yauhen Ihnatsyeu <greatdoubletbpot@gmail.com>
2018-06-27 17:03:32 +0300

    Removed app.UseHsts()

--- FS/Startup.cs
+++ FS/Startup.cs
@@ -93,14 +93,14 @@ namespace FS
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseHsts();
+            //}
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
