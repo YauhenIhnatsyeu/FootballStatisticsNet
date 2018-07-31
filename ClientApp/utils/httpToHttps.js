9c396d6abeac332c666ecca76b9dd095f86ed56a
Yauhen Ihnatsyeu <greatdoubletbpot@gmail.com>
2018-07-31 18:34:45 +0300

    Wrote a test fot httpToHttps utility.

--- ClientApp/utils/httpToHttps.js
+++ ClientApp/utils/httpToHttps.js
@@ -1,6 +1,6 @@
 export default function httpToHttps(urlParam) {
     let url = urlParam;
-    const regexp = /http:/;
+    const regexp = /^http:/;
     if (regexp.test(url)) {
         url = url.replace(regexp, "https:");
     }
