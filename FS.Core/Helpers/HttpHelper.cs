282192f6eba0adaa0ffac73cdbba85e417b29f8b
Yauhen Ihnatsyeu <greatdoubletbpot@gmail.com>
2018-08-08 11:31:52 +0300

    Added HttpHelper.

---
+++ FS.Core/Helpers/HttpHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xizmark.Webber;
+
+namespace FS.Core.Helpers
+{
+    public static class HttpHelper
+    {
+        public class HttpHelperResponse
+        {
+            public readonly int StatusCode;
+            public readonly string Response;
+
+            public HttpHelperResponse(int statusCode, string response)
+            {
+                StatusCode = statusCode;
+                Response = response;
+            }
+        }
+
+        public static HttpHelperResponse Get(string url)
+        {
+            var response = Webber.Get(url);
+            return new HttpHelperResponse(response.StatusCode, response.RawResult);
+        }
+    }
+}
