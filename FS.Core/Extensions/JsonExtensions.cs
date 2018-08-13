b2f93761026a68450caaa41aff39e1191a8e19a4
Yauhen Ihnatsyeu <greatdoubletbpot@gmail.com>
2018-08-13 11:19:08 +0300

    Added ContainsKeysTree to JsonExtensions.

---
+++ FS.Core/Extensions/JsonExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace FS.Core.Extensions
+{
+    public static class JsonExtensions
+    {
+        public static bool ContainsKeysTree(this JToken jToken, params string[] keys)
+        {
+            JToken jTokenForTesting = JToken.Parse(jToken.ToString());
+
+            foreach (string key in keys)
+            {
+                if (!(jTokenForTesting is JObject jObject))
+                {
+                    return false;
+                }
+
+                if (jObject.ContainsKey(key))
+                {
+                    jTokenForTesting = jObject[key];
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ FS.Tests/JsonExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using FS.Core.Extensions;
+using FS.Core.Utils;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace FS.Tests
+{
+    public class JsonExtensionsTests
+    {
+        [Fact]
+        public void ContainsKeysTree_ValidKeys_ReturnsTrue()
+        {
+            const bool expected = true;
+
+            JToken jToken = JToken.Parse("{\"first\":{\"second\":{\"third\":{}}}}");
+            var keys = new[] {"first", "second", "third"};
+            bool actual = jToken.ContainsKeysTree(keys);
+
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void ContainsKeysTree_InvalidKeys_ReturnsFalse()
+        {
+            const bool expected = false;
+
+            JToken jToken = JToken.Parse("{\"first\":{\"second\":{\"third\":{}}}}");
+
+            var keys = new[] { "first", "third", "second" };
+            bool actual = jToken.ContainsKeysTree(keys);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
